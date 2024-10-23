9680d5841f762df5e5b93623fef9abbfcc8ab6c3
Shabbir-Anjum <mahrshabbir768@gmail.com>
2024-10-23 17:32:56 +0500

    update

--- frontend/app/setup-assistant/page.jsx
+++ frontend/app/setup-assistant/page.jsx
@@ -14,14 +14,13 @@ const CreateAgentPage = () => {
   const dispatch = useDispatch();
   const [step, setStep] = useState(1);
   const [isSidebarCollapsed, setIsSidebarCollapsed] = React.useState(false);
-  const [voices, setVoices] = useState([]);
   const [isGeneratingAvatar, setIsGeneratingAvatar] = useState(false);
   const toggleSidebar = () => setIsSidebarCollapsed(!isSidebarCollapsed);
 
   const [agentData, setAgentData] = useState({
     name: "",
     language: "",
-    voice: "",
+    voice: "Aria",
     avatar: "",
     jobField: "",
     greeting: "",
@@ -30,13 +29,31 @@ const CreateAgentPage = () => {
     customKnowledge: "",
     files: [],
   });
+  const voices = [
+    { voice_id: '9BWtsMINqrJLrRacOk9x', name: 'Aria' },
+    { voice_id: 'CwhRBWXzGAHq8TQ4Fs17', name: 'Roger' },
+    { voice_id: 'EXAVITQu4vr4xnSDxMaL', name: 'Sarah' },
+    { voice_id: 'FGY2WhTYpPnrIDTdsKH5', name: 'Laura' },
+    { voice_id: 'IKne3meq5aSn9XLyUdCD', name: 'Charlie' },
+    { voice_id: 'JBFqnCBsd6RMkjVDRZzb', name: 'George' },
+    { voice_id: 'N2lVS1w4EtoT3dr4eOWO', name: 'Callum' },
+    { voice_id: 'SAz9YHcvj6GT2YYXdXww', name: 'River' },
+    { voice_id: 'TX3LPaxmHKxFdv7VOQHJ', name: 'Liam' },
+    { voice_id: 'XB0fDUnXU5powFXDhCwa', name: 'Charlotte' },
+    { voice_id: 'Xb7hH8MSUJpSbSDYk0k2', name: 'Alice' },
+    { voice_id: 'XrExE9yKIg1WjnnlVkGX', name: 'Matilda' },
+    { voice_id: 'bIHbv24MWmeRgasZH58o', name: 'Will' },
+    { voice_id: 'cgSgspJ2msm6clMCkdW9', name: 'Jessica' },
+    { voice_id: 'cjVigY5qzO86Huf0OWal', name: 'Eric' },
+    { voice_id: 'iP95p4xoKVk53GoZ742B', name: 'Chris' },
+    { voice_id: 'nPczCjzI2devNBz1zQrb', name: 'Brian' },
+    { voice_id: 'onwK4e9ZLuTAKqWW03F9', name: 'Daniel' },
+    { voice_id: 'pFZP5JQG7iQjIQuC4Bku', name: 'Lily' },
+    { voice_id: 'pqHfZKP75CvOlQylNhV4', name: 'Bill' }
+  ];
+
 
   const XI_API_KEY = process.env.NEXT_PUBLIC_XI_API_KEY;
-
-  useEffect(() => {
-    fetchVoices();
-  }, []);
-
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setAgentData((prevData) => ({ ...prevData, [name]: value }));
